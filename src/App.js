0bfb43187145f9e507aa025442fc0f99bc654d69
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-04-18 21:20:32 +0200

    Visual layer for static elements

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
+        <h1>Drag'n Drop</h1>
       </header>
+      <main className="dragApp">
+        <section className="dragApp__wrapper">
+          <h2 className="dragApp__title">1st container</h2>
+          <div className="dragApp__container" />
+        </section>
+        <section className="dragApp__wrapper">
+          <h2 className="dragApp__title">2st container</h2>
+          <div className="dragApp__container" />
+        </section>
+      </main>
     </div>
   );
 }
