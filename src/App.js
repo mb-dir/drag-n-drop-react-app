2761f7599929c2e3d90f043f273abfc438a5fc3e
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-04-18 21:36:15 +0200

    Dynamically generate the elements
    Simple map throught the data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import React from "react";
 import "./App.css";
+import data from "./data.js";
 
 function App() {
+  const [ stData, setStData ] = React.useState(data.elements);
+
+  const stElement = stData.map(el => {
+    return (
+      <div className="element">
+        <h3 className="element__title">{el.title}</h3>
+        <p className="element__date">{el.date}</p>
+        <p className="element__description">{el.description}</p>
+      </div>
+    );
+  });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,7 +23,7 @@ function App() {
       <main className="dragApp">
         <section className="dragApp__wrapper">
           <h2 className="dragApp__title">1st container</h2>
-          <div className="dragApp__container" />
+          <div className="dragApp__container">{stElement}</div>
         </section>
         <section className="dragApp__wrapper">
           <h2 className="dragApp__title">2st container</h2>
